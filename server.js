e9ad49ed659d1aae4afebcec5937b25e304a1bad
trofima <azarija@gmail.com>
2024-12-25 23:14:56 +0200

    primitive server

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import chalk from 'chalk'
+
+process.chdir(dirname(fileURLToPath(import.meta.url)))
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('.'))
+
+app.route('*').get((_, response) => response.render('index'))
+
+app.listen(3000, () => console.info(chalk.blue('Playground app started on port 3000')))
