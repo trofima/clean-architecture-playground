996d85e01f232870a4ab0ba93b4b81cf66fc8ef4
trofima <azarija@gmail.com>
2024-12-27 17:20:46 +0200

    native example core integration

--- packages/native-example/server.js
+++ packages/native-example/server.js
@@ -1,16 +1,45 @@
 import express from 'express'
 import {fileURLToPath} from 'url'
 import {dirname} from 'path'
+import {rollup} from 'rollup'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import chalk from 'chalk'
 
 process.chdir(dirname(fileURLToPath(import.meta.url)))
+const staticFilesRegExp = /.+\..+/
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './entrypoints')
 app.use(express.static('.'))
+app.use(async (request, __, next) => {
+  if (!staticFilesRegExp.test(request.path)) {
+    console.info(chalk.blue('\nbuilding client...'))
+
+    try {
+      const bundle = await rollup({
+        input: './src/index.js',
+        plugins: [nodeResolve()],
+      })
+
+      await bundle.write({
+        format: 'es',
+        name: 'bundle',
+        file: 'dist/native-example-app.js',
+        sourcemap: true,
+      })
+    } catch (e) {
+      console.error(chalk.red(`\nbuilding client failed because of\n${e}`))
+      return next()
+    }
+
+    console.info(chalk.green('built client successfully'))
+  }
+
+  return next()
+})
 
 app.route('*').get((_, response) => response.render('index'))
 
-app.listen(3000, () => console.info(chalk.blue('Playground app started at http://localhost:3000/')))
+app.listen(3000, () => console.info(chalk.blue('Native Example app started on port 3000')))
