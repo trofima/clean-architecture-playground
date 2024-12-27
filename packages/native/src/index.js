4d451673458af0a915ef4cf7cc20eccd9c83027e
trofima <azarija@gmail.com>
2024-12-27 15:27:36 +0200

    lil rename

--- packages/native/src/index.js
+++ packages/native/src/index.js
@@ -1,4 +1,4 @@
-import {RenderOrderList} from 'clean-architecture-playground-core'
+import {RenderOrderList} from '@clean-architecture-playground/core'
 // import {RenderOrderList} from '../../core/src/index.js'
 
 console.log(111, RenderOrderList)
