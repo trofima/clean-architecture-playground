560ff8b4cdb1d4578b3d677e3218e892aa759f5b
LiashkoIvan <ivan.theglyph@gmail.com>
2025-01-03 11:36:37 +0200

    add view to order card

--- packages/native-example/src/order/view.js
+++ packages/native-example/src/order/view.js
@@ -2,21 +2,28 @@ export const renderOrderView = ({id, createdDate, updatedDate, user, sum, paymen
   <link rel="stylesheet" href="src/order/view.css">
 
   <div class="order-page">
-    <div><label>Order Id: </label> ${id}</div>
-    <div><label>Created Date: </label> ${createdDate}</div>
-    <div><label>Updated Date: </label> ${updatedDate}</div>
-    <div><label>User Name: </label> ${user}</div>
-    <div><label>Sum: </label> ${sum}</div>
-    <div><label>Payment Status: </label> ${paymentStatus}</div>
-    <div><label>Fulfillment Status: </label> ${fulfillmentStatus}</div>
-    <div><label>Shipping Address: </label> ${shippingAddress}</div>
-    <div><label>Billing Address: </label> ${billingAddress}</div>
+    <div class="title">Parameters of Order No ${id}</div>
+    <div class="order-parameters">
+      <div><label>Order Id: </label> ${id}</div>
+      <div><label>Created Date: </label> ${createdDate}</div>
+      <div><label>Updated Date: </label> ${updatedDate}</div>
+      <div><label>User Name: </label> ${user}</div>
+      <div><label>Sum: </label> ${sum}</div>
+      <div><label>Payment Status: </label> ${paymentStatus}</div>
+      <div><label>Fulfillment Status: </label> ${fulfillmentStatus}</div>
+      <div><label>Shipping Address: </label> ${shippingAddress}</div>
+      <div><label>Billing Address: </label> ${billingAddress}</div>
+    </div>
   </div>
 `
 
 export const renderOrderLoadingView = () => `
+  <link rel="stylesheet" href="src/order/view.css">
+
   <div class="order-page">
-    <div>Loading...</div>
+    <div class="loading-state">
+      <p>Loading...</p>
+    </div>
   </div>
 `
 
