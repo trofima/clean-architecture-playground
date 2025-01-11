8ec448869a962a4ebc08cf17eeec353d90d586a6
trofima <azarija@gmail.com>
2025-01-11 13:13:36 +0200

    lil

--- packages/core/src/order-list/use-cases/render-order-list.js
+++ packages/core/src/order-list/use-cases/render-order-list.js
@@ -2,5 +2,5 @@ import {OrderListPresentation} from '../entities/order-list-presentation.js'
 
 export const RenderOrderList = ({presentation, updateOrderList}) => async () => {
   presentation.update(() => OrderListPresentation.make({loading: false, limit: 5, offset: 0, total: 0, list: []}))
-  updateOrderList()
+  await updateOrderList()
 }
