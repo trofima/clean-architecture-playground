9b243db2079d2d5ccc88371e6ec8251ca8954269
trofima <azarija@gmail.com>
2025-01-17 20:42:19 +0200

    core: lil

--- packages/core/src/order/use-cases/close-order.js
+++ packages/core/src/order/use-cases/close-order.js
@@ -2,7 +2,10 @@ export const CloseOrder = ({presentation, navigator, notifier}) => async () => {
   const {hasChanges} = presentation.get()
   if (
     !hasChanges
-    || await notifier.confirm('Changes will be lost. Are you sure you want to close this order?', {type: 'warning'})
+    || await notifier.confirm(
+        'Changes will be lost. Are you sure you want to close this order?',
+        {type: 'warning'},
+      )
   ) {
     await navigator.close()
   }
