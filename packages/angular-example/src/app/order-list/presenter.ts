7a61a9b0722b71a04d40ccf4df745fc9491ccac2
trofima <azarija@gmail.com>
2025-04-04 16:51:17 +0300

    angular: change presenter's file extensions

--- packages/angular-example/src/app/order-list/presenter.ts
+++ packages/angular-example/src/app/order-list/presenter.ts
@@ -1,17 +1,17 @@
-export const presentOrderList = (presentation) => ({
+export const presentOrderList = (presentation: any) => ({
   ...presentation,
-  list: presentation.list.map(({createdDate, sum, ...rest}) => ({
+  list: presentation.list.map(({createdDate, sum, ...rest}: any) => ({
     ...rest,
     createdDate: formatDate(createdDate),
     sum: formatSum(sum),
   })),
 })
 
-const formatDate = (isoDate) => {
+const formatDate = (isoDate: string) => {
   if (!isoDate) return ''
   const [date, time] = isoDate.split('T')
   const [hours, minutes] = time.split(':')
   return `${date}, ${hours}:${minutes}`
 }
 
-const formatSum = (sum) => Number(sum).toFixed(2).toString()
+const formatSum = (sum: number) => Number(sum).toFixed(2).toString()
